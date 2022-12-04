e67e1680c081213dec995bce90e1ab54b30e3885
davidalantodd <david.todd@multiverse.io>
2022-12-03 23:39:14 -0500

    solution for d3p2

--- day-3/main.rb
+++ day-3/main.rb
@@ -5,10 +5,14 @@
 #initilize variables
 priority_sum = 0
 similar = ''
+lines = Array.new
 
 # read file and loop through each line
 File.foreach("data.txt") {
     |line|
+
+    #store lines in array for part 2, remove \n char
+    lines << line[0,line.length-1]
 
     #split into two rucksacks/strings
     len = line.length
@@ -26,11 +30,28 @@ File.foreach("data.txt") {
     similar.downcase == similar ?
         (priority_sum += similar.ord - 96) :
         (priority_sum += similar.ord - 38)
-
 }
 
-puts priority_sum
+#part 2 -- iterate through each line in sets of 3 to compare for similar value
+#new variables
+badge = ''
+badge_sum = 0
 
+lines.each_slice(3){
+    |elf1, elf2, elf3|
+    elf1.each_char{
+        |letter|
+        if (elf2.include?(letter) && elf3.include?(letter))
+            badge = letter
+        end
+    }
+    badge.downcase == badge ?
+        (badge_sum += badge.ord - 96) :
+        (badge_sum += badge.ord - 38)
+}
+
+puts "Part 1 sum: " + String(priority_sum)
+puts "Part 2 sum: " + String(badge_sum)
 
 
 
